8a8fd97770a05c749390fbe77e1a25fb22292ac9
Tulaev Muhammad <99670987+TulaevMuhammad@users.noreply.github.com>
2023-07-21 11:54:45 +0500

    init

---
+++ src/main/java/tmd/render/Customer.java
@@ -0,0 +1,13 @@
+package tmd.render;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
